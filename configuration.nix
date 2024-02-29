2539be430c83bcb3f02dd0a5e80defe992f00b70
Dylan Tompkins <dltompki@outlook.com>
2024-02-28 19:06:47 -0800

    2024-02-28T19:06:47-08:00

--- configuration.nix
+++ configuration.nix
@@ -179,14 +179,6 @@
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
   };
-  environment.etc."wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-    bluez_monitor.properties = {
-      ["bluez5.enable-sbc-xq"] = true,
-      ["bluez5.enable-msbc"] = true,
-      ["bluez5.enable-hw-volume"] = true,
-      ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-    }
-  '';
 
   # fish shell
   programs.fish.enable = true;
