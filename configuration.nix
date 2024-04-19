a6de9bc0ae5f6023a2d712dfd9d309f842ece58d
Dylan Tompkins <dltompki@outlook.com>
2024-04-18 20:02:21 -0700

    feat(tuigreet): remember username and add asterisks

--- configuration.nix
+++ configuration.nix
@@ -286,7 +286,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -tr --asterisks --cmd Hyprland";
         user = "dylan";
       };
     };
