2546225d4a09daff7e1e6564c19b947cef3a0c68
Dylan Tompkins <dltompki@outlook.com>
2024-04-20 17:30:00 -0700

    fix: switch vt for tuigreet
    so that systemd doesn't overwrite it

--- configuration.nix
+++ configuration.nix
@@ -284,6 +284,7 @@
 
   services.greetd = {
     enable = true;
+    vt = 2;
     settings = {
       default_session = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet -tr --asterisks --cmd Hyprland";
