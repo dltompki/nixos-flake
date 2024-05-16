ad8174f15b5066afd79ee19ab4f8d277ff8e79a7
Dylan Tompkins <dltompki@outlook.com>
2024-05-16 16:19:38 -0700

    feat: wezterm visual bell

--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -20,5 +20,14 @@ config.window_background_opacity = 0.9
 -- Temporary fix since wayland support is broken
 config.enable_wayland = false
 
+-- Replace audible bell with visual one
+config.audible_bell = "Disabled";
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 150,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 150,
+}
+
 -- and finally, return the configuration to wezterm
 return config
