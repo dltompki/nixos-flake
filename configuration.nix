436c39cf529f3ef5a4171504a74046666b394189
Dylan Tompkins <dltompki@outlook.com>
2024-12-31 22:22:29 -0800

    feat: add power management from laptop wiki page
    https://wiki.nixos.org/wiki/Laptop

--- configuration.nix
+++ configuration.nix
@@ -298,6 +298,20 @@
   };
 
   powerManagement.enable = true;
+  services.thermald.enable = true;
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+    };
+  };
 
   services.asusd.enable = true;
 
